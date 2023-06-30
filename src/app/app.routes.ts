55f0249b72b5cd6b520a322f1dfb23cd492abea6
Carlos Bayarri <e_cbayar@mercadona.com>
2023-06-30 09:53:54 +0200

    feat: second commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Route } from '@angular/router';
+import { NxWelcomeComponent } from './nx-welcome.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    component: NxWelcomeComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'products',
+    loadComponent: () =>
+      import('@super-angular-nx-app/modules/products').then(
+        (m) => m.ProductsComponent
+      ),
+  },
+  {
+    path: 'orders',
+    loadComponent: () =>
+      import('@super-angular-nx-app/modules/orders').then(
+        (m) => m.OrdersComponent
+      ),
+  },
+];
